cc4920eeaf65ecb57b76ec88954045ad8a972a82
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-18 07:03:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //your JS code here. If required.
 let input = document.querySelector(".input");
 let btn = document.querySelector(".btn");
+let div = document.querySelector(".search");
 
 btn.addEventListener('click', () =>{
-	input.classList.add('active');
+	input.classList.add('focused');
+	div.classList.add('active');
 })
