a294a892fe6ff0b163f750a6b8a64eabfd39eb73
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-18 07:23:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-let input = document.querySelector(".input");
+let input = document.querySelector("input");
 let btn = document.querySelector("button");
 let div = document.querySelector(".search");
 
