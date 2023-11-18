83eaaf5f64446622beb553eb155f2d77ffb77a64
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-18 06:58:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let input = document.querySelector(".input");
+let btn = document.querySelector(".btn");
+
+btn.addEventListener('click', () =>{
+	input.classList.add('active');
+})
